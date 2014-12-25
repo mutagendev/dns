5a61a40b9c379808ebde73c1bae9226a5e072ef5
Jon Chen <bsd@voltaire.sh>
2014-12-25 15:50:47 -0500

    unjank envvars

--- dns.py
+++ dns.py
@@ -3,14 +3,12 @@ from libcloud.dns.providers import DRIVERS, get_driver as get_dns_driver
 from libcloud.dns.types import Provider as DNSProvider
 
 
-def provider_related(environ_key):
-    for provider in DRIVERS:
-        if environ_key.startswith(provider.upper()):
-            return True
-    return False
-
-
-_KEYS = {key: os.environ[key] for key in os.environ if provider_related(os.environ[key])}
+def env_vars():
+    env_vars = {}
+    for key in os.environ:
+        if key.startswith('DNS_'):
+            env_vars[key[4:]] = os.getenv(key)
+    return env_vars
 
 if __name__ == '__main__':
     print(True)
