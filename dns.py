c6901da4e4e47afd10f27aa01ed958d860503562
Jon Chen <bsd@voltaire.sh>
2014-12-25 16:12:37 -0500

    map of maps

--- dns.py
+++ dns.py
@@ -7,7 +7,17 @@ def env_vars():
     env_vars = {}
     for key in os.environ:
         if key.startswith('DNS_'):
-            env_vars[key[4:]] = os.getenv(key)
+            provider = key[4:].split('_')[0].lower()
+            if provider not in env_vars:
+                if provider in DRIVERS:
+                    env_vars[provider] = {}
+                elif key[4:].startswith('RACKSPACE_UK'):
+                    provider = 'rackspace_uk'
+                    env_vars['rackspace_uk'] = {}
+                elif key[4:].startswith('RACKSPACE_US'):
+                    provider = 'rackspace_us'
+                    env_vars['rackspace_us'] = {}
+            env_vars[provider] = os.getenv(key)
     return env_vars
 
 if __name__ == '__main__':
