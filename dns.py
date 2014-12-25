cc1a4fda0d8f6bfc9a0bf06f2164d43ed13bb8ac
Jon Chen <bsd@voltaire.sh>
2014-12-25 02:03:17 -0500

    crazy crap

---
+++ dns.py
@@ -0,0 +1,16 @@
+import os
+from libcloud.dns.providers import DRIVERS, get_driver as get_dns_driver
+from libcloud.dns.types import Provider as DNSProvider
+
+
+_KEYS = {k, v for key, os.environ[key] in os.environ if provider_related(os.environ[key])}
+
+
+def provider_related(environ_key):
+    for provider in DRIVERS:
+        if environ_key.startswith(provider):
+            return True
+    return False
+
+
+if __name__ == '__main__':
